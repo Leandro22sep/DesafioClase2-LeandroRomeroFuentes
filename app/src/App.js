fd5b2bc2120164a3a962ac328470d8432dc6a367
LeandroRomeroFuentes <leandroromerofuentes@gmail.com>
2022-07-23 15:01:48 -0300

    primer commit

---
+++ app/src/App.js
@@ -0,0 +1,16 @@
+import NavBar from "./components/NavBar";
+function App(){
+    return (
+        <>
+            <header id="header">
+                <h1 className="titulo">Titulo</h1>
+                <NavBar />
+            </header>
+            <main>
+
+            </main>
+        </>
+    )
+}
+
+export default App;
---
+++ app/src/components/NavBar.js
@@ -0,0 +1,11 @@
+function NavBar() {
+    return (
+        <nav>
+            <a href="#">link</a>
+            <a href="#">link</a>
+            <a href="#">link</a>
+        </nav>
+    )
+}
+
+export default NavBar;
